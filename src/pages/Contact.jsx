03e41b9ce11254f5ab7731968e26b51bc00652be
nonso7 <felixmedia78@gmail.com>
2022-11-05 13:03:17 -0700

    fix ui

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -16,7 +16,7 @@ export default function Contact() {
           <div className="flex flex-col md:flex-row gap-4 w-full">
             <div className="w-full">
               <label
-                htmlFor="password"
+                htmlFor="first_name"
                 className="block mb-2 text-sm font-medium text-gray-900"
               >
                 First name
@@ -29,9 +29,9 @@ export default function Contact() {
                 required
               />
             </div>
-            <div className="w-full">
+            <div className="w-full mt-4 sm:mt-0">
               <label
-                htmlFor="repeat-password"
+                htmlFor="last_name"
                 className="block mb-2 text-sm font-medium text-gray-900"
               >
                 Last name
@@ -62,13 +62,12 @@ export default function Contact() {
           </div>
           <div>
             <label
-              htmlFor="email"
+              htmlFor="message"
               className="block mb-2 text-sm font-medium text-gray-900"
             >
               Message
             </label>
             <textarea
-              name=""
               id="message"
               className="shadow-sm border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 focus-visible:ring-blue-500 focus-visible:border-blue-500 block w-full p-2.5"
               cols="30"
